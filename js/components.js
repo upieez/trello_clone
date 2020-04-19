e396d1721f26c4b7bd0ac19f246d9b930172337f
upieez <samuelhww@gmail.com>
2020-04-19 20:13:23 +0800

    feat: add function to change attribute

--- js/components.js
+++ js/components.js
@@ -8,46 +8,15 @@ class DisplayBoard extends HTMLElement {
         let templateContent = template.content;
 
         this.columnsTemplate = document.createElement('display-columns');
-        this.columnsTemplate.setAttribute("id", "column-" + number )
+        this.columnsTemplate.setAttribute("id", "column-" + number );
+        this.columnsTemplate.setAttribute("content", "Titleeeeeeee");
 
         this.cardsTemplate = document.createElement('display-cards');
-        this.cardsTemplate.setAttribute("id", "card-" + number )
+        this.cardsTemplate.setAttribute("id", "card-" + number );
 
         this.shadow = this.attachShadow({mode: 'open'});
         this.shadow.appendChild(templateContent.cloneNode(true));
 
-    }
-
-    columnsData(){
-        const responseHandler = function() {
-            console.log("response text", JSON.parse(this.responseText));
-            console.log("status text", this.statusText);
-            console.log("status code", this.status);
-          };
-          
-          const request = new XMLHttpRequest();
-          
-          request.addEventListener("load", responseHandler);
-          const url = "http://localhost:3000/columns";
-          request.open("GET", url);
-          
-          request.send();
-    }
-
-    cardsData(){
-        const responseHandler = function() {
-            console.log("response text", JSON.parse(this.responseText));
-            console.log("status text", this.statusText);
-            console.log("status code", this.status);
-          };
-          
-          const request = new XMLHttpRequest();
-          
-          request.addEventListener("load", responseHandler);
-          const url = "http://localhost:3000/cards";
-          request.open("GET", url);
-          
-          request.send();
     }
 
     connectedCallback(){
@@ -57,27 +26,33 @@ class DisplayBoard extends HTMLElement {
         let cards = this.shadowRoot.querySelector("#column-1").shadowRoot.querySelector("#card")
             cards.appendChild(this.cardsTemplate);        
         
-        this.columnsData();
-        this.cardsData();
         this.createColumns();
+        this.createCards();
     }
 
     createColumns(){
-        const columnsTemplate = document.createElement('display-columns')
-        let button = this.shadowRoot.querySelector("#add-button")
+        let button = this.shadowRoot.querySelector("#add-column")
         let form = this.shadowRoot.querySelector("#form")
-        
         button.addEventListener("click", () => {
-            form.parentNode.insertBefore(columnsTemplate,form);
+            form.parentNode.insertBefore(document.createElement('display-columns'),form);
         })
     }
 
+    createCards(){
+        let button = this.shadowRoot.querySelector("#column-1")
+                    .shadowRoot.querySelector("#add-card")
+        let column = this.shadowRoot.querySelector("#column-1")
+                    .shadowRoot.querySelector("#card")
+        
+        button.addEventListener("click", () => {
+            column.appendChild(document.createElement('display-cards'));
+        })
+    }
 }
 
 class DisplayColumns extends HTMLElement {
     constructor(){
         super();
-
         let template = document.getElementById('display-columns');
         let templateContent = template.content;
 
@@ -85,19 +60,46 @@ class DisplayColumns extends HTMLElement {
         this.shadow.appendChild(templateContent.cloneNode(true));
     }
 
-    static get observedAttributes() { 
-        console.log("is this working")
-        return ["column-header-1","column-header", "column-name"]; 
+    get content() {
+        return this.getAttribute("content");
     }
 
+    set content(value){
+        this.setAttribute("content", value);
+    }
+
+    static get observedAttributes() {return ["content"];}
+
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name == 'column-header-1' || name == 'column-header' || name == "column-name"){
-            console.log(name);
-            console.log("this is the old value ", oldValue);
-            console.log("this is the new value ", newValue)
-            console.log('attributes changed.');
-        }
-      }
+        console.log(name, oldValue, newValue);
+        this.render();
+        this.shadowRoot.querySelector(".column-name")
+        .addEventListener('focus',this.saveTitle.bind(this));
+    }
+
+    saveTitle(){
+        this.addEventListener("blur", function(){
+            document.querySelector("body > display-board")
+                    .shadowRoot.querySelector("#column-1")
+                    .setAttribute("content", document.querySelector("body > display-board")
+                    .shadowRoot.querySelector("#column-1")
+                    .shadowRoot.querySelector("div > div.column-header > div")
+                    .innerText)
+        })
+    }
+    
+    connectedCallback(){
+        this.render();
+        this.shadowRoot.querySelector(".column-name")
+            .addEventListener('focus',this.saveTitle.bind(this));
+    }
+
+    render(){
+        let columnHeader = this.shadowRoot.querySelector(".column-header")
+        columnHeader.innerHTML = `
+                <div class="column-name" slot="column-name" contenteditable>${this.content}</div>
+        `
+    }
 }
 
 class DisplayCards extends HTMLElement {
@@ -109,6 +111,11 @@ class DisplayCards extends HTMLElement {
 
         this.shadow = this.attachShadow({mode: 'open'});
         this.shadow.appendChild(templateContent.cloneNode(true));
+    }
+    
+    connectedCallback(){
+        let cardName = this.shadowRoot.querySelector(".cards");
+        cardName.innerHTML = `<div slot="card-name"> WOWOWOWOWOWOWO </div>`
     }
 }
 
