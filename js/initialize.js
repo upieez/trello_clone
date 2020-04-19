a8798ebb64fca0f1bb298463403c2822ead1a424
upieez <samuelhww@gmail.com>
2020-04-20 02:29:50 +0800

    feat: add function to read card from server

--- js/initialize.js
+++ js/initialize.js
@@ -2,24 +2,56 @@ window.addEventListener("DOMContentLoaded", () => {
 
     var board = document.querySelector(".board-rows");
 
-    var responseHandler = function() {
-        var result = JSON.parse(this.responseText);
-        console.log(result.length);
-        let x = 0;
-        while (x < result.length) {
-            var column = document.createElement('display-column')
-            column.setAttribute('title', result[x]["title"]);
-            board.prepend(column);
-            x++;
-        }
-    };
-    
-    var request = new XMLHttpRequest();
-    
-    request.addEventListener("load", responseHandler);
-    
-    var url = "http://localhost:3000/columns";
-    request.open("GET", url);
-    
-    request.send();
+    const loadCloumns = () => {
+        var responseHandler = function() {
+            var result = JSON.parse(this.responseText);
+            console.log(result.length);
+            var x = 0;
+            while (x < result.length) {
+                var column = document.createElement('display-column')
+                column.setAttribute('id', "column-" + (x + 1));
+                column.setAttribute('title', result[x]["title"]);
+                board.prepend(column);
+                x++;
+            }
+        };
+        
+        var request = new XMLHttpRequest();
+        var url = "http://localhost:3000/columns";
+        
+        request.addEventListener("load", responseHandler);
+        request.open("GET", url);
+        request.send();
+    }
+
+    const loadCards = () => {
+        var responseHandler = function() {
+            var result = JSON.parse(this.responseText);
+            console.log(result.length);
+            var x = 0;
+            while (x < result.length) {
+                var card = document.createElement('display-card')
+                card.setAttribute('slot', result[x]["title"]);
+                card.setAttribute('title', result[x]["title"]);
+
+                var column = document.querySelector("#column-"+result[x]["columnId"]);
+                console.log(column);
+                var columnCard = column.shadowRoot.querySelector("#column-cards");
+                columnCard.appendChild(card);
+                x++;
+            }
+        };
+        
+        var request = new XMLHttpRequest();
+        var url = "http://localhost:3000/cards";
+        
+        request.addEventListener("load", responseHandler);
+        request.open("GET", url);
+        request.send();
+    }
+
+
+    loadCloumns();
+    loadCards();
+
 })
